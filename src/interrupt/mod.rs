c486ff351c75009f2fd44fa42cd2850926a3c766
dcas796 <89193854+dcas796@users.noreply.github.com>
2026-07-24 20:53:29 +0200

    Implement an extensible way to initialize the IDT

--- src/interrupt/mod.rs
+++ src/interrupt/mod.rs
@@ -1,31 +1,9 @@
-use x86::dtables::{lidt, DescriptorTablePointer};
-use x86::Ring;
-use x86::segmentation::{SegmentSelector, SystemDescriptorTypes32};
-use crate::interrupt::entry::IdtEntry;
-use crate::interrupt::stack_frame::InterruptStackFrame;
-use crate::logln;
-use crate::util::unsafe_wrappers::UnsafeSyncSend;
+use x86::dtables::lidt;
+use crate::interrupt::table::IDTR;
 
 pub mod entry;
 mod stack_frame;
-
-const CODE_DESCRIPTOR_INDEX: u16 = 1;
-
-lazy_static! {
-    static ref IDT: [u64; 256] = [IdtEntry::new(
-        interrupt_handler as *const () as u32,
-        SegmentSelector::new(CODE_DESCRIPTOR_INDEX, Ring::Ring0),
-        SystemDescriptorTypes32::InterruptGate32,
-        Ring::Ring0
-    ).into_u64(); 256];
-
-    static ref IDTR: UnsafeSyncSend<DescriptorTablePointer<u64>> =
-        DescriptorTablePointer::new_from_slice(&*IDT).into();
-}
-
-extern "x86-interrupt" fn interrupt_handler(stack_frame: InterruptStackFrame) {
-    logln!("Interrupt received: {stack_frame}");
-}
+mod table;
 
 pub fn init_interrupts() {
     unsafe {
---
+++ src/interrupt/table.rs
@@ -0,0 +1,95 @@
+use x86::dtables::DescriptorTablePointer;
+use crate::interrupt::stack_frame::InterruptStackFrame;
+use crate::logln;
+use crate::util::unsafe_wrappers::UnsafeSyncSend;
+
+macro_rules! table {
+    ($(
+        #[int($n:literal)]
+        $(#[$attr:meta])*
+        extern "x86-interrupt" fn $name:ident($($arg:tt)*) {
+            $($body:tt)*
+        }
+    )*
+    $(
+        default $dname:ident for $dn:literal
+    )*) => {
+        $(
+            $(#[$attr])*
+            extern "x86-interrupt" fn $name($($arg)*) {
+                $($body)*
+            }
+        )*
+
+        $(
+            extern "x86-interrupt" fn $dname(stack_frame: InterruptStackFrame) {
+                use $crate::logln;
+                logln!("Interrupt {:#02x} received: {stack_frame}", $dn);
+            }
+        )*
+
+        lazy_static! {
+            static ref IDT: [u64; 256] = {
+                use $crate::interrupt::entry::IdtEntry;
+                use x86::segmentation::{SegmentSelector, SystemDescriptorTypes32};
+                use x86::Ring;
+
+                const CODE_DESCRIPTOR_INDEX: u16 = 1;
+
+                let mut table = [0; 256];
+                $(
+                    table[$n] = IdtEntry::new(
+                        $name as *const () as u32,
+                        SegmentSelector::new(CODE_DESCRIPTOR_INDEX, Ring::Ring0),
+                        SystemDescriptorTypes32::InterruptGate32,
+                        Ring::Ring0
+                    ).into_u64();
+                )*
+                $(
+                    table[$dn] = IdtEntry::new(
+                        $dname as *const () as u32,
+                        SegmentSelector::new(CODE_DESCRIPTOR_INDEX, Ring::Ring0),
+                        SystemDescriptorTypes32::InterruptGate32,
+                        Ring::Ring0
+                    ).into_u64();
+                )*
+                table
+            };
+        }
+    };
+}
+
+lazy_static! {
+    pub static ref IDTR: UnsafeSyncSend<DescriptorTablePointer<u64>> =
+        DescriptorTablePointer::new_from_slice(&*IDT).into();
+}
+
+table! {
+    #[int(0x80)]
+    extern "x86-interrupt" fn int_80(stack_frame: InterruptStackFrame) {
+        logln!("Interrupt 0x80 (syscall) received: {stack_frame}");
+    }
+
+    default int_00 for 0x00
+    default int_01 for 0x01
+    default int_02 for 0x02
+    default int_03 for 0x03
+    default int_04 for 0x04
+    default int_05 for 0x05
+    default int_06 for 0x06
+    default int_07 for 0x07
+    default int_08 for 0x08
+    default int_09 for 0x09
+    default int_0a for 0x0a
+    default int_0b for 0x0b
+    default int_0c for 0x0c
+    default int_0d for 0x0d
+    default int_0e for 0x0e
+    default int_0f for 0x0f
+    default int_10 for 0x10
+    default int_11 for 0x11
+    default int_12 for 0x12
+    default int_13 for 0x13
+    default int_14 for 0x14
+    default int_15 for 0x15
+}
