4d46eda7a09f986256354fd9b2d42a43abf6bee6
dcas796 <89193854+dcas796@users.noreply.github.com>
2026-07-31 14:06:14 +0200

    Add check whether interrupts were already disabled for `InterruptGuard`

--- src/util/interrupt_guard.rs
+++ src/util/interrupt_guard.rs
@@ -1,15 +1,27 @@
+use x86::bits32::eflags::EFlags;
+
 #[derive(Debug, Clone)]
-pub struct InterruptGuard;
+pub struct InterruptGuard {
+    were_interrupts_disabled: bool,
+}
 
 impl InterruptGuard {
     pub fn new() -> Self {
-        unsafe { x86::irq::disable(); }
-        Self
+        let were_interrupts_disabled = !unsafe { x86::bits32::eflags::read() }
+            .contains(EFlags::FLAGS_IF);
+        if !were_interrupts_disabled {
+            unsafe { x86::irq::disable() };
+        }
+        Self {
+            were_interrupts_disabled
+        }
     }
 }
 
 impl Drop for InterruptGuard {
     fn drop(&mut self) {
-        unsafe { x86::irq::enable(); }
+        if !self.were_interrupts_disabled {
+            unsafe { x86::irq::enable() };
+        }
     }
 }
