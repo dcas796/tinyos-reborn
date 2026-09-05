114b662ff86262789cc4e64bd3e9608ea3ff228b
dcas796 <89193854+dcas796@users.noreply.github.com>
2026-09-05 11:15:02 +0200

    Add `PathBuf`

--- src/fs/path.rs
+++ src/fs/path.rs
@@ -1,3 +1,9 @@
+use alloc::borrow::ToOwned;
+use alloc::fmt;
+use alloc::string::String;
+use core::borrow::Borrow;
+use core::fmt::Formatter;
+
 #[repr(transparent)]
 pub struct Path {
     inner: str,
@@ -17,5 +23,38 @@ impl Path {
 
     pub fn is_absolute(&self) -> bool {
         self.inner.starts_with("/")
+    }
+}
+
+impl ToOwned for Path {
+    type Owned = PathBuf;
+
+    fn to_owned(&self) -> Self::Owned {
+        PathBuf::new(self.inner.to_owned())
+    }
+}
+
+#[derive(Clone)]
+pub struct PathBuf {
+    inner: String,
+}
+
+impl PathBuf {
+    pub const fn new(s: String) -> Self {
+        Self {
+            inner: s
+        }
+    }
+}
+
+impl fmt::Debug for PathBuf {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        self.inner.fmt(f)
+    }
+}
+
+impl Borrow<Path> for PathBuf {
+    fn borrow(&self) -> &Path {
+        Path::new(self.inner.borrow())
     }
 }
